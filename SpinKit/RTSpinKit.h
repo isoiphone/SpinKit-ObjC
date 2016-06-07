ec3474ab8d8b9554f1f9dd8cccdaf301925a22d2
jacob_schwartz <jacob@glint.io>
2016-06-07 14:13:46 -0700

    correctly include header

--- SpinKit/RTSpinKit.h
+++ SpinKit/RTSpinKit.h
@@ -15,4 +15,4 @@ FOUNDATION_EXPORT double RTSpinKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char RTSpinKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <RTSpinKit/PublicHeader.h>
-#import "SpinKit/RTSpinKitView.h"
+#import <RTSpinKit/RTSpinKitView.h>
